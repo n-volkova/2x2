36d32918e5441ec8d82d14f07bd9f09efe81fed8
Nadia Martynova <nadezhda.martynova@breffi.ru>
2020-07-11 00:44:43 +0300

    publicPath fixed

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     productionSourceMap: false,
-    transpileDependencies: ['standardized-audio-context']
+    transpileDependencies: ['standardized-audio-context'],
+    publicPath: process.env.NODE_ENV === 'production' ? '/2x2/' : '/'
 }
